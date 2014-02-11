c092f80bc5cb4ed103e5d8606310a2f157752119
CaylakPenguen <caylakpenguen@gmail.com>
2014-02-12 01:22:38 +0200

    Bismillahirrahmanirrahim

---
+++ bundles/001-core/rootfs/etc/profile.d/lang.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Set the system locale.  (no, we don't have a menu for this ;-)
+# For a list of locales which are supported by this machine, type:
+#   locale -a
+
+#export LANG=en_US.UTF-8
+export LANG=tr_TR.UTF-8
