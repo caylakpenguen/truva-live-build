f3c40af00639aebdcbe004a3348a8c0993e4269d
CaylakPenguen <caylakpenguen@gmail.com>
2014-03-16 20:32:04 +0000

    make_iso.sh edit

--- cdrom/truva/make_iso.sh
+++ cdrom/truva/make_iso.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 # ---------------------------------------------------
-# Script to create bootable ISO in Linux
-# usage: make_iso.sh [ /tmp/slax.iso ]
-# author: Tomas M. <http://www.linux-live.org>
+# Script to create bootable ISO in Truva Linux
+# Date: 16 - 03 - 2014
+# author: CaylakPenguen <http://www.truvalinux.org.tr>
 # ---------------------------------------------------
 
-if [ "$1" = "--help" -o "$1" = "-h" ]; then
-  echo "This script will create bootable ISO from files in curent directory."
-  echo "Current directory must be writable."
-  echo "example: $0 /tmp/truva.iso"
-  exit
-fi
-
 CDLABEL="Truva Linux Live CDROM"
-ISONAME="$1"
-
-if [ "$ISONAME" = "" ]; then
-   SUGGEST="../../`basename \`pwd\``.iso"
-   SUGGEST="`readlink -f $SUGGEST`"
-   echo -ne "Target ISO file name [ Hit enter for $SUGGEST ]: "
-   read ISONAME
-   if [ "$ISONAME" = "" ]; then ISONAME="$SUGGEST"; fi
-fi
-
-mkisofs -o "$ISONAME" -v -J -R -D -A "Truva Linux 2.0" -V "$CDLABEL" \
+ISODIR="/tmp"
+ISONAME= "truva-`uname -m`-`date +%F`.iso"
+HEDEF="$ISODIR/$ISONAME"
+cd ..
+mkisofs -o "$HEDEF" -v -J -R -D -A "Truva Linux" -V "$CDLABEL" \
 -no-emul-boot -boot-info-table -boot-load-size 4 \
--b truva/boot/isolinux.bin -c truva/boot/isolinux.boot ../.
+-b truva/boot/isolinux.bin -c truva/boot/isolinux.boot .
+
+echo " Md5Sum Dosyasi Hazırlaniyor."
+cd $ISODIR
+
+md5sum $ISONAME >$ISONAME.md5sum
+
+echo " Islem Tamamlandi."
