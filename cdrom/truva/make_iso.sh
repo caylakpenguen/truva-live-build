e4b2adb78b5e15b349e49d4af65d99cfcf534e85
CaylakPenguen <caylakpenguen@gmail.com>
2014-03-18 00:40:24 +0000

    make_iso.sh fix

--- cdrom/truva/make_iso.sh
+++ cdrom/truva/make_iso.sh
@@ -7,16 +7,17 @@
 
 CDLABEL="Truva Linux Live CDROM"
 ISODIR="/tmp"
-ISONAME= "truva-`uname -m`-`date +%F`.iso"
+ISONAME=truva-`uname -m`-`date +%F`.iso
 HEDEF="$ISODIR/$ISONAME"
+
 cd ..
 mkisofs -o "$HEDEF" -v -J -R -D -A "Truva Linux" -V "$CDLABEL" \
 -no-emul-boot -boot-info-table -boot-load-size 4 \
 -b truva/boot/isolinux.bin -c truva/boot/isolinux.boot .
 
-echo " Md5Sum Dosyasi Hazırlaniyor."
-cd $ISODIR
+echo " Md5Sum Dosyasi Hazirlaniyor."
 
+cd $ISODIR
 md5sum $ISONAME >$ISONAME.md5sum
 
 echo " Islem Tamamlandi."
